a6309dcd8f137c019644cb808f7c9f6c66dac631
kayrsant <kayrsant@gmail.com>
2022-07-20 22:05:02 -0300

    factoring and updating code as in the video lesson

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -20,37 +14,37 @@ public class App {
          Integer opcaoEscolhida = lerOpcao.nextInt();
 
          String url = CreateUrl.setUrl(opcaoEscolhida);
-         HttpClient client = HttpClient.newHttpClient();
-         URI endereco = URI.create(url);
-         var request = HttpRequest.newBuilder(endereco).GET().build();
-         HttpResponse < String > response = client.send(request, BodyHandlers.ofString());
-         String body = response.body();
-         var parser = new JsonParser();
-         List < Map < String, String >> listaDeFilmes = parser.parse(body);
+
+         ClienteHttp http = new ClienteHttp();
+         String json = http.buscaDados(url);
+
+         ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+         List<Conteudo> conteudos = extrator.extraiConteudos(json);
+        
          GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
 
-         for (Map < String, String > filme: listaDeFilmes) {
+         for (int i = 0; i < conteudos.size(); i++) {
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
-            String nomeArquivo = titulo.replace(":", "-") + ".png";
+            Conteudo conteudo = conteudos.get(i);
+            String nomeArquivo = conteudo.getTitulo().replace(":", " -") + ".png";
+           
 
             try {
-               InputStream inputStream = new URL(urlImagem).openStream();
-               System.out.println("Gerando imagem - [" + titulo + "]");
+               InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+               System.out.println("Gerando imagem - [" + conteudo.getTitulo() + "]");
                geradora.cria(inputStream, nomeArquivo);
             } catch (java.io.FileNotFoundException err) {
                System.out.println("Imagem não encontrada ou link inválido");
             }
 
-            System.out.println("\033[0;41m" + "Nome do filme:" + "\033[0;1m " + titulo + RESET);
-            String notaDoFilme = filme.get("imDbRating");
+            System.out.println("\033[0;41m" + "Nome do filme:" + "\033[0;1m " + conteudo.getTitulo() + RESET);
+            /*String notaDoFilme = conteudo.get("imDbRating");
             System.out.println("\033[44m" + "Nota do filme:" + "\033[0;1m " + notaDoFilme);
             String stars = "";
             for (int i = 0; i < Math.round(Float.parseFloat(notaDoFilme)); i++) {
                stars += "\u2B50";
             }
-            System.out.println(stars);
+            System.out.println(stars); */
             System.out.println();
          }
       } catch (Exception e) {
